0a8942fd6b08e97e1a052a2a44b6663ac5018278
Wen Kokke <wenkokke@users.noreply.github.com>
2021-12-25 19:01:58 +0000

    Minor changes

--- src/STLC.hs
+++ src/STLC.hs
@@ -48,19 +48,31 @@ import Data.Coolean (Cool, Coolean (toCool), false, (&&&), (==>))
 
 -- $finiteTypes
 --
--- Finite types are natural numbers which are bounded on the type level by a type-level natural number. We use finite types throughout this development to represent variables as deBruijn indices, to get a type of expressions which are well-scoped by construction.
+-- This section implements finite types, which we will use to represent variables in deBruijn notation.
 --
--- Finite types can be encoded in Haskell '98 using a pair of datatypes, 'Z' and 'S', which represent the constructors of natural numbers on the type level. Together, these datatypes construct the number /bounding/ the finite type, e.g., @'S' ('S' 'Z')@ represents the number 2, and as a type it has two inhabitants, @'FZ'@ and @'FS' 'FZ'@, the numbers smaller than two.
+-- Finite types are a family of types which represent the natural numbers smaller than some number /n/.
+-- Finite types can be encoded in Haskell '98 using a pair of datatypes, 'Z' and 'S', which represent the number 0 and the successor function.
+-- These are used to represent the number /bounding/ the finite type, /e.g./, @('S' ('S' 'Z'))@ represents the number 2, and as such it is the type of all natural numbers smaller than 2, /i.e./, 0 and 1, or 'FZ' and @('FS' 'FZ')@.
 
--- | The empty type with zero inhabitants.
+-- | The type of natural numbers smaller than 0.
+--
+--   There are no such numbers, so there are no values of type 'Z'.
 data Z
   deriving (Eq, Ord, Show)
 
--- | The successor type with one more inhabitant than its type argument /n/. For example, 'Z' has zero inhabitants, so @'S' 'Z'@ has one inhabitant, 'FZ', and @'S' ('S' 'Z')@ has two inhabitants, 'FZ' and @'FS' 'FZ'@, /etc./
+-- | The type of natural numbers smaller than @n + 1@..
+--   For example:
+--
+--   * There are no values of type 'Z'.
+--   * There is one value of type @('S' 'Z')@, 'FZ'.
+--     We cannot use 'FS' at this type, as it needs an argument of type 'Z'.
+--   * There are two values of type @('S' ('S' 'Z'))@, 'FZ' and @('FS' 'FZ')@.
+--     We /can/ use 'FS' at this type, as it needs an argument of type @('S' 'Z')@, and there is one such value, 'FZ'.
+--   * /Etc./
 data S n
   = -- | The number @0@.
     FZ
-  | -- | The successor function, i.e., @'FS' n@ represents @n + 1@.
+  | -- | The successor function. @('FS' n)@ represents @(n + 1)@.
     FS n
   deriving (Eq, Ord, Functor)
 
@@ -76,10 +88,10 @@ fromS _ fs (FS n) = fs n
 -- | This instance is needed to satisfy instant search, but it implements no functions.
 instance Num Z
 
--- | This instance is needed to satisfy the superclass constraint on integral, but it implements no functions.
+-- | This instance is needed to satisfy the superclass constraint on 'Integral', but it implements no functions.
 instance Real Z
 
--- | This instance is needed to satisfy the superclass constraint on integral, but it implements no functions.
+-- | This instance is needed to satisfy the superclass constraint on 'Integral', but it implements no functions.
 instance Enum Z
 
 -- | This instance is needed to satisfy instant search, but it implements no functions.
@@ -92,10 +104,10 @@ instance Num n => Num (S n) where
   fromInteger 0 = FZ
   fromInteger n = FS (fromInteger (n - 1))
 
--- | This instance is needed to satisfy the superclass constraint on integral, but it implements no functions.
+-- | This instance is needed to satisfy the superclass constraint on 'Integral', but it implements no functions.
 instance Real n => Real (S n)
 
--- | This instance is needed to satisfy the superclass constraint on integral, but it implements no functions.
+-- | This instance is needed to satisfy the superclass constraint on 'Integral', but it implements no functions.
 instance Enum n => Enum (S n)
 
 -- | This instance allows us to convert deBruijn indices to integers.
@@ -114,6 +126,8 @@ instance Enumerable n => Enumerable (S n) where
   enumerate = share . aconcat $ [c0 FZ, c1 FS]
 
 -- $typesAndExpressions
+--
+-- This section implements simple types and well-scoped by construction expressions, as well as a simple type checker which we will use to generate well-typed expressions the [lazy-search](https://hackage.haskell.org/package/lazy-search) package.
 
 -- | The type of simple types.
 data Type
@@ -125,18 +139,20 @@ data Type
 
 deriveEnumerable ''Type
 
--- | The type of expressions with /i/ free variables.
+-- | The type of expressions with @i@ free variables.
 --
---   Commonly, /i/ will be instantiated with a finite type, e.g., @'Expr' ('S' ('S' 'Z'))@ is the type of expressions with two free variables, @'FZ'@ and @'FS' 'FZ'@.
---   These finite types are interpreted as deBruijn indices. For example:
+--   We instantiate @i@ with a finite type which representing the number of free variables, /e.g./, @'Expr' ('S' ('S' 'Z'))@ is the type of expressions with two free variables.
+--   That way, when we construct a variable using 'Var', we only have @i@ options, and they all refer to one of the free variables.
+--   These finite types are interpreted as deBruijn indices, which means that an index @i@ refer to the @i@/th/ lambda you encounter when traversing the expression.
+--   For example:
 --
---   +---------------------------+---------------------------+-----------------------+-------------------------------------+
---   | Haskell function          | as \(\lambda\)-expression | in DeBruijn notation  | as value of type @'Expr' 'Z'@       |
---   +===========================+===========================+=======================+=====================================+
---   | @\\x -> x@                | \(\lambda x.x\)           | \(\lambda.0\)         | @'Lam' ('Var' 'FZ')@                |
---   +---------------------------+---------------------------+-----------------------+-------------------------------------+
---   | @\\x -> \\y -> x@         | \(\lambda x.\lambda y.x\) | \(\lambda.\lambda.1\) | @'Lam' ('Lam' ('Var' ('FS' 'FZ')))@ |
---   +---------------------------+---------------------------+-----------------------+-------------------------------------+
+--   +---------------------------+---------------------------+--------------------------+-------------------------------------+
+--   | Haskell expression        | \(\lambda\)-expression    | using DeBruijn notation  | as value of type @'Expr' 'Z'@       |
+--   +===========================+===========================+==========================+=====================================+
+--   | @\\x -> x@                | \(\lambda x.x\)           | \(\lambda.0\)            | @'Lam' ('Var' 'FZ')@                |
+--   +---------------------------+---------------------------+--------------------------+-------------------------------------+
+--   | @\\x -> \\y -> x@         | \(\lambda x.\lambda y.x\) | \(\lambda.\lambda.1\)    | @'Lam' ('Lam' ('Var' ('FS' 'FZ')))@ |
+--   +---------------------------+---------------------------+--------------------------+-------------------------------------+
 data Expr i
   = -- | A variable.
     Var i
@@ -146,7 +162,7 @@ data Expr i
     Lam (Expr (S i))
   | -- | An application.
     --
-    --   Function applications store the type of their argument so that type checking can be parallelized, i.e., in the case for 'App', 'check' can immediately recur into both sub-expressions, rather than having to 1/st/ infer the type of the argument. This is important for efficient enumeration using 'Control.Search.search' and related functions.
+    --   Function applications store the type of their argument so that type checking can be parallelized, /i.e./, in the case for 'App', 'check' can immediately recur into both sub-expressions, rather than having to 1/st/ infer the type of the argument. This is important for efficient enumeration using the [lazy-search](https://hackage.haskell.org/package/lazy-search-0.1.2.1) package.
     App (Expr i) (Expr i) Type
   deriving (Eq, Show, Functor)
 
@@ -164,7 +180,7 @@ type Ctx i = i -> Type
 (|>) :: Ctx i -> Type -> Ctx (S i)
 (|>) = flip fromS
 
--- | Check if an expression has the given type under the given typing context. The algorithm is straightforward, modulo the conversion between 'Bool' and concurrent booleans, i.e., 'Cool', which is omitted here:
+-- | Check if an expression has the given type under the given typing context. The algorithm is straightforward, modulo the conversion between 'Bool' and concurrent booleans, /i.e./, 'Cool', which is omitted here:
 --
 -- > check ctx a         (Var i)       = a == ctx i
 -- > check ctx (a :-> b) (Lam e)       = check (ctx |> a) b e
@@ -178,9 +194,9 @@ check ctx b (App e1 e2 a) = check ctx (a :-> b) e1 &&& check ctx a e2
 
 -- $normalizationBySubstitution
 
--- | The type of substitutions from expressions with /i/ free variables to expressions with /j/ free variables.
+-- | The type of substitutions from expressions with @i@ free variables to expressions with /j/ free variables.
 --
---   Substitutions are total maps from deBruijn indices bound by /i/ to expressions with /j/ free variables.
+--   Substitutions are total maps from deBruijn indices bound by @i@ to expressions with /j/ free variables.
 type Sub i j = i -> Expr j
 
 -- | Extend a substitution with another variable, which is mapped to itself.
@@ -203,7 +219,7 @@ sub s = \case
 sub0 :: Expr i -> Expr (S i) -> Expr i
 sub0 e = sub (fromS e Var)
 
--- | The pattern of redexes, i.e., \((\lambda x.e_1)\;e_2\) or @'App' ('Lam' e1) e2 t@.
+-- | The pattern of redexes, /i.e./, \((\lambda x.e_1)\;e_2\) or @'App' ('Lam' e1) e2 t@.
 pattern Red :: Expr (S i) -> Expr i -> Type -> Expr i
 pattern Red e1 e2 t = App (Lam e1) e2 t
 
@@ -225,6 +241,8 @@ step = \case
       <|> (App e1 <$> step e2 <*> pure t)
 
 -- | Reduce the expression using 'step' until no more redexes exist.
+--
+--   Implements normalization by substitution.
 nbs :: Expr i -> Expr i
 nbs e = maybe e nbs (step e)
 
